6210d44f76a68037c7acfa0d3fc16cd180797537
p-w-g <p.grzelczyk@gmail.com>
2024-06-24 14:56:36 +0200

    moves logic to context _layout

--- app/login.tsx
+++ app/login.tsx
@@ -1,73 +1,40 @@
-import { View, Text, TextInput, Button, Alert } from 'react-native';
-import React, { useContext, useRef, useState } from 'react';
-import { AuthContext } from './_layout';
-import { firebase_auth } from '../firebaseConfig';
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from 'firebase/auth';
-// import { Redirect, router } from 'expo-router';
+import { View, Text, TextInput, Button } from "react-native";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "./_layout";
+import { router } from "expo-router";
 
 export default function login() {
-  const { setUserToken } = useContext(AuthContext);
-
-  const [userName, setUserName] = useState('');
-  const [passWord, setPassWord] = useState('');
-
-  const auth = firebase_auth;
-
-  const handleLogin = async () => {
-    if (userName === '' || !userName) return;
-    if (passWord === '' || !passWord) return;
-
-    signInWithEmailAndPassword(auth, userName, passWord)
-      .then((user) => setUserToken(user))
-      .then(() => router.push('/createBingo'))
-      .catch((error) => {
-        if (error.code === 'auth/email-already-in-use')
-          return Alert.alert('That email address is already in use!');
-
-        if (error.code === 'auth/invalid-email')
-          return Alert.alert('That email address is invalid!');
-
-        Alert.alert(`Miscellaneous error: ${error.code}`);
-      });
-  };
-
-  const handleSignup = async () => {
-    if (userName === '' || !userName) return;
-    if (passWord === '' || !passWord) return;
-
-    await createUserWithEmailAndPassword(auth, userName, passWord)
-      .then((user) => setUserToken(user))
-      .catch((error) => {
-        if (error.code === 'auth/email-already-in-use')
-          return Alert.alert('That email address is already in use!');
-
-        if (error.code === 'auth/invalid-email')
-          return Alert.alert('That email address is invalid!');
-
-        Alert.alert(`Miscellaneous error: ${error.code.split('auth/')[0]}`);
-      });
-  };
+  const { setUserToken, createUser } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [passWord, setPassWord] = useState("");
 
   return (
     <View>
       <Text>login</Text>
       <TextInput
-        placeholder="Type in your email"
-        value={userName}
-        onChangeText={setUserName}
+        placeholder='Type in your email'
+        value={email}
+        onChangeText={setEmail}
       />
       <TextInput
-        placeholder="Type in your password"
+        placeholder='Type in your password'
         value={passWord}
         onChangeText={setPassWord}
         secureTextEntry
       />
-      <Button title="Login" onPress={() => handleLogin()} />
+      <Button
+        title='Login'
+        onPress={() => (
+          setUserToken(email, passWord), router.replace("/createBingo")
+        )}
+      />
       <Text>Or</Text>
-      <Button title="Sign up" onPress={() => handleSignup()} />
+      <Button
+        title='Sign up'
+        onPress={() => (
+          createUser(email, passWord), router.replace("/createBingo")
+        )}
+      />
     </View>
   );
 }
