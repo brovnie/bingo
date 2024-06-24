6660cb98cc51d98f13b05db896e6a33cf37e3cc5
p-w-g <p.grzelczyk@gmail.com>
2024-06-24 15:12:11 +0200

    scaffold login logic in layout since the authcontext lives there

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,42 +1,98 @@
-import { FirebaseAuthTypes } from '@react-native-firebase/auth';
-import { Slot, Stack } from 'expo-router';
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, View, StyleSheet } from 'react-native';
+import { FirebaseAuthTypes } from "@react-native-firebase/auth";
+import { Slot } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
+import { firebase_auth } from "../firebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 type AuthContextType = {
   userToken: FirebaseAuthTypes.UserCredential | null;
-  setUserToken: React.Dispatch<
-    React.SetStateAction<FirebaseAuthTypes.UserCredential | null>
-  >;
+  setUserToken: (email: string, pw: string) => void;
+  createUser: (email: string, pw: string) => void;
+  clearUserToken: (userId: string) => void;
+  isLoading: boolean;
 };
 
 export const AuthContext = React.createContext<AuthContextType>({
   userToken: null,
-  setUserToken: () => {},
+  setUserToken: (email: string, pw: string) => null,
+  createUser: (email: string, pw: string) => null,
+  clearUserToken: (userId: string) => null,
+  isLoading: true,
 });
 
 export default function RootLayout() {
   const [userToken, setUserToken] =
     useState<FirebaseAuthTypes.UserCredential | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const auth = firebase_auth;
 
   useEffect(() => {
-    // Simulate token retrieval
-    setTimeout(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) setUserToken(user);
       setIsLoading(false);
-    }, 1000);
+    });
   }, []);
 
-  if (isLoading) {
-    return (
-      <View style={styles.loadingContainer}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
-  }
+  const handleLogin = async (email: string, pw: string) => {
+    const [userName, setUserName] = useState("");
+    const [passWord, setPassWord] = useState("");
 
-  return (
-    <AuthContext.Provider value={{ userToken, setUserToken }}>
+    const auth = firebase_auth;
+
+    const handleLogin = async () => {
+      if (userName === "" || !userName) return;
+      if (passWord === "" || !passWord) return;
+
+      await signInWithEmailAndPassword(auth, userName, passWord)
+        .then((user) => setUserToken(user))
+        .catch((error) => {
+          if (error.code === "auth/email-already-in-use")
+            return Alert.alert("That email address is already in use!");
+
+          if (error.code === "auth/invalid-email")
+            return Alert.alert("That email address is invalid!");
+
+          Alert.alert(`Miscellaneous error: ${error.code}`);
+        });
+    };
+  };
+
+  const handleSignup = async (email: string, pw: string) => {
+    if (userName === "" || !userName) return;
+    if (passWord === "" || !passWord) return;
+
+    await createUserWithEmailAndPassword(auth, userName, passWord)
+      .then((user) => setUserToken(user))
+      .catch((error) => {
+        if (error.code === "auth/email-already-in-use")
+          return Alert.alert("That email address is already in use!");
+
+        if (error.code === "auth/invalid-email")
+          return Alert.alert("That email address is invalid!");
+
+        Alert.alert(`Miscellaneous error: ${error.code.split("auth/")[0]}`);
+      });
+  };
+  const handleLogOut = async (userId: string) => {};
+
+  return isLoading ? (
+    <View style={styles.loadingContainer}>
+      <ActivityIndicator size='large' />
+    </View>
+  ) : (
+    <AuthContext.Provider
+      value={{
+        userToken,
+        setUserToken: () => handleLogin(email, pw),
+        createUser: () => handleSignup(email, pw),
+        clearUserToken: () => handleLogOut(userId),
+      }}
+    >
       <Slot />
     </AuthContext.Provider>
   );
@@ -44,8 +100,8 @@ export default function RootLayout() {
 const styles = StyleSheet.create({
   loadingContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   container: {
     flex: 1,
